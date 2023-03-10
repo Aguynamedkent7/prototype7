91002d519e3a33456c48fa84a1fda0f9526597f9
Aguynamedkent7 <butaya.kentvincent07@gmail.com>
2023-03-10 23:25:32 +0800

    bro am ded

--- proto/lib/main.dart
+++ proto/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
 
 void main() {
   runApp(const Myapp());
@@ -17,49 +18,83 @@ class Myapp extends StatefulWidget {
 }
 
 class _MyappState extends State<Myapp> {
+  late MapZoomPanBehavior _panBehavior;
+  late Random random;
+  late List<MapLatLng> polyline;
+  late MapZoomPanBehavior _mapZoomPanBehavior;
+  late List<PolylineModel> polylines;
+
+  @override
+  void initState() {
+    polyline = const <MapLatLng>[
+      MapLatLng(8.475474, 124.648183),
+      MapLatLng(8.475649, 124.649513),
+      MapLatLng(8.477628, 124.648445),
+      MapLatLng(8.477368, 124.649668),
+    ];
+
+    polylines = <PolylineModel>[
+      PolylineModel(polyline),
+    ];
+    _mapZoomPanBehavior = MapZoomPanBehavior(
+        zoomLevel: 15,
+        focalLatLng: const MapLatLng(8.475649, 124.649513),
+        enableDoubleTapZooming: true);
+
+    random = Random();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final TextStyle textStyle = themeData.textTheme.caption!
+        .copyWith(color: themeData.colorScheme.surface);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(8.47743, 124.65153),
-            zoom: 15,
-          ),
-          nonRotatedChildren: [
-            AttributionWidget.defaultWidget(
-              source: 'OpenStreetMap contributors',
-              onSourceTapped: null,
-            ),
-          ],
-          children: [
-            TileLayer(
+        body: SfMaps(
+          layers: [
+            MapTileLayer(
               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
-            ),
-            ListView(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding:
-                          const EdgeInsets.symmetric(vertical: (15.0)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: const BorderSide(width: 0.8),
+              zoomPanBehavior: _mapZoomPanBehavior,
+              sublayers: [
+                MapPolylineLayer(
+                  polylines: List<MapPolyline>.generate(
+                    polylines.length,
+                    (int index) {
+                      return MapPolyline(
+                        points: polylines[index].points,
+                      );
+                    },
+                  ).toSet(),
+                  color: Colors.green,
+                  width: 5,
+                  tooltipBuilder: (BuildContext context, int index) {
+                    return Container(
+                      padding: EdgeInsets.only(left: 15, top: 10),
+                      width: 170,
+                      height: 50,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('Jeep Name  :  ', style: textStyle),
+                              Text('Jeep ni Arn', style: textStyle),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text('Travel time    :  ', style: textStyle),
+                              Text(random.nextInt(30).toString() + ' mins',
+                                  style: textStyle),
+                            ],
+                          ),
+                        ],
                       ),
-                      hintText: 'Search a Destination',
-                      prefixIcon: const Icon(
-                        Icons.search,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                ),
+                    );
+                  },
+                )
               ],
             ),
           ],
@@ -89,4 +124,9 @@ class _MyappState extends State<Myapp> {
       ),
     );
   }
+}
+
+class PolylineModel {
+  PolylineModel(this.points);
+  final List<MapLatLng> points;
 }
